402cacddc14706132358a694fe0fef2a63c57041
Smith Ellis <smithe@gmail.com>
2013-01-02 16:49:10 -0500

    Better formatting of dictionary

--- ipdefine.py
+++ ipdefine.py
@@ -1,2 +1,5 @@
 #List out the devices and ip addresses you'd like to check
-ip_address = {'Router': '192.168.0.1', 'Phone': '172.18.125.223'}
+ip_address = {
+	'Router': '192.168.0.1', 
+	'Phone': '192.168.0.2',
+}
