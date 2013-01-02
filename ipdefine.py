694f104e17da72b54f82b5e15b3552045653c7e5
Smith Ellis <smithe@gmail.com>
2013-01-02 15:49:52 -0500

    Allow multiple IPs in separate module

---
+++ ipdefine.py
@@ -0,0 +1,2 @@
+#List out the ip addresses you'd like to check
+ip_address = ['192.168.0.1','172.18.125.223']
--- pybell.py
+++ pybell.py
@@ -1,13 +1,13 @@
 import sys, os, socket
-
-#Our Phone has a known, assigned IP address
-ip_address = '192.168.0.148'
+#ipdefine contains the list of IP addresses we want to check
+import ipdefine
 
 #Let's see if the Phone is on the network right now
-try:
-    socket.gethostbyaddr(ip_address)
-    # If previous line doesn't throw exception, IP address is being used; let's perform some action
-    print "Phone on network"
-except socket.herror:
-    # socket.gethostbyaddr() throws error, so IP is not being used at present; Phone not on network
-    print "Phone not on network"
+for ipadd in ipdefine.ip_address:
+	try:
+	    socket.gethostbyaddr(ipadd)
+	    # If previous line doesn't throw exception, IP address is being used; let's perform some action
+	    print "Phone on network"
+	except socket.herror:
+	    # socket.gethostbyaddr() throws error, so IP is not being used at present; Phone not on network
+	    print "Phone not on network"
