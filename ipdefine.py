3aa68d5cb08f5fc39462926c7f570d336f3c21cc
Smith Ellis <smithe@gmail.com>
2013-01-03 18:27:05 -0500

    Learning git...ug

--- ipdefine.py
+++ ipdefine.py
@@ -1,6 +1,11 @@
 #List out the devices and ip addresses you'd like to check
 ip_address = {
+<<<<<<< HEAD
 	'Router': '192.168.0.1',
 	'WorkTop': '192.168.0.3', 
 	'Phone': '192.168.0.2',
+=======
+	'Router': '192.168.0.1', 
+	'Phone': '192.168.0.250',
+>>>>>>> Learning git...ug
 }
