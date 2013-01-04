ea531b024da76428f1af8c45a30258b7a2840820
Smith Ellis <smithe@gmail.com>
2013-01-04 10:02:29 -0500

    Corrected files

--- ipdefine.py
+++ ipdefine.py
@@ -1,5 +1,6 @@
 #List out the devices and ip addresses you'd like to check
 ip_address = {
-	'Router': '192.168.0.1', 
+	'Router': '192.168.0.1',
+	'WorkTop': '192.168.0.3', 
 	'Phone': '192.168.0.2',
 }
