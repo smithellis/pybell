18bbc493d2e21a86270d29204fbb8a8a3520ea30
Smith Ellis <smithe@gmail.com>
2013-01-03 19:18:46 -0500

    Revert "Moved from sockets to subprocess"
    This reverts commit 33042fca329842c4631d60f14bb13771958e6b1c.

--- ipdefine.py
+++ ipdefine.py
@@ -1,6 +1,5 @@
 #List out the devices and ip addresses you'd like to check
 ip_address = {
-	'Router': '192.168.0.1',
-	'WorkTop': '192.168.0.3', 
+	'Router': '192.168.0.1', 
 	'Phone': '192.168.0.2',
 }
--- pybell.py
+++ pybell.py
@@ -1,23 +1,13 @@
-#import sys, os, socket
+import sys, os, socket
 #ipdefine contains the list of IP addresses we want to check
-import subprocess
 import ipdefine
 
 #Let's see if the device is on the network right now
-#for device, ipadd in ipdefine.ip_address.iteritems():
-#	try:
-#	    socket.gethostbyaddr(ipadd)
-#	    # If previous line doesn't throw exception, IP address is being used; let's perform some action
-#	    print device+" on network"
-#	except socket.herror:
-#	    # socket.gethostbyaddr() throws error, so IP is not being used at present; device not on network
-#	    print device+" not on network"
-
 for device, ipadd in ipdefine.ip_address.iteritems():
-	res = subprocess.call(['ping','-n','1',ipadd],stdout=False)
-	if res == 0:
-		print device+" is on network."
-	elif res == 2:
-		print device+" is not responding."
-	else: 
-		print "Ping of "+device+" failed."
+	try:
+	    socket.gethostbyaddr(ipadd)
+	    # If previous line doesn't throw exception, IP address is being used; let's perform some action
+	    print device+" on network"
+	except socket.herror:
+	    # socket.gethostbyaddr() throws error, so IP is not being used at present; device not on network
+	    print device+" not on network"
